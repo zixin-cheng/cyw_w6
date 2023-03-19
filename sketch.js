7f3a2dd1c8f715f5bbadb3fd7a97206cf0725c16
zixin-cheng <janecheng1026@gmail.com>
2023-03-19 00:53:02 -0400

    v2

--- sketch.js
+++ sketch.js
@@ -1,253 +1,106 @@
-let x, y, w, h;
+let resolution = 0.1;
+
+let x1 = 0;
+let y1 = 0;
+let x2ran = 0;
+let y2ran = 0;
+let x2 = 0;
+let y2 = 0;
+
+let pts;
+let font;
+
+function preload(){
+  font = loadFont('RobotoMono-Thin.ttf');
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  
   strokeWeight(1);
-  stroke(255);
   noFill();
+  stroke(255);
+  frameRate(10);
+  
+  xposition = width/5;
+  yposition = height/2;
 }
 
 function draw() {
   background(0);
+
+  drawText("C", font,width*0.20,height/2);
+  drawText("O", font,width*0.40,height/2);
+  drawText("D", font,width*0.60,height/2);
+  drawText("E", font,width*0.80,height/2);
   
-  x = width/10;
-  w = mouseX/10;
-  y = height/2;
-  h = mouseY/5;
-
-  drawC(x*2, y, w, h);
-  drawO(x*4, y, w, h);
-  drawD(x*6, y, w, h);
-  drawE(x*8, y, w, h);
+  // noLoop();
 }
 
-function drawC(xCord, yCord, x, y) {
-  push();
-  translate(xCord, yCord)
-  beginShape();
-  vertex(40 + x, -60 - y);
-  vertex(20 + x, -40 - y);
-  vertex(30 + x, -90 - y);
-  vertex(10 + x, -70 - y);
-  vertex(20 + x, -90 - y);
-  vertex(-10, -80 - y);
-  vertex(-20 - x, -100 - y);
-  vertex(-20 - x, -70 - y);
-  vertex(-30 - x, -80 - y);
-  vertex(-30 - x, -50 - y);
-  vertex(-50 - x, -40 - y);
-  vertex(-30 - x, -20 - y);
-  vertex(-50 - x, -20 - y);
-  vertex(-30 - x, 20);
-  vertex(-30 - x, 20);
-  vertex(-40 - x, 40 + y);
-  vertex(-20 - x, 60 + y);
-  vertex(-30 - x, 60 + y);
-  vertex(-40 - x, 80 + y);
-  vertex(-20 - x, 80 + y);
-  vertex(-10 - x, 110 + y);
-  vertex(0, 90 + y);
-  vertex(10 + x, 100 + y);
-  vertex(10 + x, 80 + y);
-  vertex(40 + x, 100 + y);
-  vertex(20 + x, 60 + y);
-  vertex(40 + x, 40 + y);
-  vertex(20 + x, 40 + y);
-  vertex(40 + x, 80 + y);
-  vertex(10 + x, 70 + y);
-  vertex(20 + x, 100 + y);
-  vertex(0, 80 + y);
-  vertex(0 - x, 110 + y);
-  vertex(-10 - x, 80 + y);
-  vertex(-20 - x, 100 + y);
-  vertex(-40 - x, 60 + y);
-  vertex(-30 - x, 50 + y);
-  vertex(-50 - x, 30 + y);
-  vertex(-20 - x, -10 + y);
-  vertex(-50 - x, 0);
-  vertex(-40 - x, -40 - y);
-  vertex(-50 - x, -60 - y);
-  vertex(-30 - x, -50 - y);
-  vertex(-40 - x, -70 - y);
-  vertex(-20 - x, -60 - y);
-  vertex(-30 - x, -90 - y);
-  vertex(-10 - x, -70 - y);
-  vertex(0, -90 - y);
-  vertex(0 + x, -80 - y);
-  vertex(40 + x, -80 - y);
-  vertex(20 + x, -50 - y);
-  endShape(CLOSE);
-  pop();
-}
 
-function drawO(xCord, yCord, x, y) {
-  push();
-  translate(xCord, yCord)
-  beginShape();
-  vertex(-20 - x, -60 - y);
-  vertex(-50 - x, -60 - y);
-  vertex(-20 - x, -30 - y);
-  vertex(-40 - x, -20 - y);
-  vertex(-30 - x, 0);
-  vertex(-40 - x, 20 + y);
-  vertex(-30 - x, 50 + y);
-  vertex(-50 - x, 70 + y);
-  vertex(-30 - x, 70 + y);
-  vertex(-30 - x, 100 + y);
-  vertex(-20 - x, 90 + y);
-  vertex(10, 110 + y);
-  vertex(10 + x, 90 + y);
-  vertex(20 + x, 100 + y);
-  vertex(30 + x, 70 + y);
-  vertex(40 + x, 50 + y);
-  vertex(20 + x, 30 + y);
-  vertex(40 + x, 20 + y);
-  vertex(30 + x, 10);
-  vertex(40 + x, -20 - y);
-  vertex(30 + x, -30 - y);
-  vertex(40 + x, -60 - y);
-  vertex(20 + x, -60 - y);
-  vertex(30 + x, -80 - y);
-  vertex(10 + x, -70 - y);
-  vertex(0 + x, -100 - y);
-  vertex(-10, -80 - y);
-  vertex(-20 - x, -60 - y);
-  vertex(-20 - x, -80 - y);
-  vertex(-10, -70 - y);
-  vertex(20 + x, -90 - y);
-  vertex(20 + x, -70 - y);
-  vertex(30 + x, -60 - y);
-  vertex(50 + x, -40 - y);
-  vertex(20 + x, -40 - y);
-  vertex(40 + x, -30);
-  vertex(30 + x, -20 + y);
-  vertex(50 + x, 0 + y);
-  vertex(20 + x, 10 + y);
-  vertex(30 + x, 50 + y);
-  vertex(50 + x, 70 + y);
-  vertex(10 + x, 80 + y);
-  vertex(30 + x, 90 + y);
-  vertex(-10, 100 + y);
-  vertex(-20 - x, 80 + y);
-  vertex(-40 - x, 80 + y);
-  vertex(-30 - x, 60 + y);
-  vertex(-50 - x, 50 + y);
-  vertex(-20 - x, 40 + y);
-  vertex(-40 - x, 40 + y);
-  vertex(-30 - x, 20 + y);
-  vertex(-40 - x, y);
-  vertex(-20 - x, -10 + y);
-  vertex(-40 - x, -10 + y);
-  vertex(-20 - x, -40 + y);
-  vertex(-40 - x, -40);
-  vertex(-50 - x, -70);
-  endShape(CLOSE);
-  pop();
-}
+function drawText(char,fontChoice,x,y){
+  
+  for(let i = 0; i< 1; i++){
+  let x1s = [];
+  let y1s = [];
+  let x1rans = [];
+  let y1rans = [];
+  let x1s2 = [];
+  let y1s2 = [];
+  let x1rans2 = [];
+  let y1rans2 = [];
+  let minDimension = min(width, height);
+  let tSize = minDimension/4;
+  let mid = textWidth(char);
+    
+  pts = fontChoice.textToPoints(char, 0,0,tSize,{
+    sampleFactor: resolution,
+    simplifyThreshold: 0
+  });  
 
-function drawD(xCord, yCord, x, y) {
-  push();
-  translate(xCord, yCord)
-  beginShape();
-  vertex(-10 - x, -70 - y);
-  vertex(-10, -100 - y);
-  vertex(10 + x, -70 - y);
-  vertex(30 + x, -80 - y);
-  vertex(30 + x, -60 - y);
-  vertex(50 + x, -40 - y);
-  vertex(20 + x, -30 - y);
-  vertex(50 + x, 20);
-  vertex(20 + x, 30 + y);
-  vertex(40 + x, 80 + y);
-  vertex(10 + x, 80 + y);
-  vertex(0 + x, 100 + y);
-  vertex(10, 90 + y);
-  vertex(-20 - x, 110 + y);
-  vertex(-30 - x, 100 + y);
-  vertex(-30 - x, 80 + y);
-  vertex(-20 - x, 40 + y);
-  vertex(-50 - x, 60 + y);
-  vertex(-30 - x, 10 + y);
-  vertex(-40 - x, -10);
-  vertex(-20 - x, -10 - y);
-  vertex(-40 - x, -60 - y);
-  vertex(-20 - x, -50 - y);
-  vertex(-50 - x, -100 - y);
-  vertex(-10 - x, -70 - y);
-  vertex(-50 - x, -80 - y);
-  vertex(-20 - x, -60 - y);
-  vertex(-30 - x, -50 - y);
-  vertex(-40 - x, -30 - y);
-  vertex(-20 - x, -30 - y);
-  vertex(-30 - x, -y);
-  vertex(-40 - x, 10 - y);
-  vertex(-30 - x, 30);
-  vertex(-50 - x, 30 + y);
-  vertex(-20 - x, 70 + y);
-  vertex(-40 - x, 70 + y);
-  vertex(-30 - x, 90 + y);
-  vertex(-50 - x, 110 + y);
-  vertex(-10, 100 + y);
-  vertex(20 + x, 90 + y);
-  vertex(30 + x, 70 + y);
-  vertex(40 + x, 60 + y);
-  vertex(20 + x, 60 + y);
-  vertex(30 + x, 20 + y);
-  vertex(50 + x, 10 + y);
-  vertex(30 + x, 0);
-  vertex(30 + x, -40 - y);
-  vertex(40 + x, -70 - y);
-  vertex(20 + x, -70 - y);
-  vertex(20 + x, -90 - y);
-  endShape(CLOSE);
-  pop();
-}
+  for(let i = 0; i < pts.length; i++){
+    push();
+    translate(x-tSize*0.3,y);
+    
+    if (i === 0) {
+    x2ran = random(20);
+    y2ran = random(20);
+    x1 = pts[i].x + x2ran;
+    y1 = pts[i].y + y2ran;
+    x2 = pts[i+1].x;
+    y2 = pts[i+1].y;
+    }
+    
+    if (i < pts.length-1){
+    x2ran = random(20);
+    y2ran = random(20);
 
-function drawE(xCord, yCord, x, y) {
-  push();
-  translate(xCord, yCord)
-  beginShape();
-  vertex(40 + x, -80 - y);
-  vertex(0 + x, -70 - y);
-  vertex(-10, -100 - y);
-  vertex(-20 - x, -80 - y);
-  vertex(-50 - x, -90 - y);
-  vertex(-30 - x, -60 - y);
-  vertex(-50 - x, -60 - y);
-  vertex(-30 - x, -30 - y);
-  vertex(-40 - x, - y);
-  vertex(-40 - x, 10);
-  vertex(-50 - x, 30 + y);
-  vertex(-30 - x, 50 + y);
-  vertex(-50 - x, 70 + y);
-  vertex(-20 - x, 80 + y);
-  vertex(-50 - x, 110 + y);
-  vertex(-20 - x, 90 + y);
-  vertex(-10, 110 + y);
-  vertex(0 + x, 90 + y);
-  vertex(40 + x, 100 + y);
-  vertex(30 + x, 80 + y);
-  vertex(0, 110 + y);
-  vertex(-40 - x, 90 + y);
-  vertex(-30 - x, 60 + y);
-  vertex(-40 - x, 50 + y);
-  vertex(-30 - x, 30 + y);
-  vertex(-50 - x, 10);
-  vertex(-20 - x, 0);
-  vertex(0, 20);
-  vertex(0 + x, -10);
-  vertex(30 + x, 10);
-  vertex(20, -10);
-  vertex(10, 0);
-  vertex(-20 - x, 10);
-  vertex(-50 - x, -20);
-  vertex(-30 - x, -10 - y);
-  vertex(-40 - x, -70 - y);
-  vertex(-20 - x, -70 - y);
-  vertex(-30 - x, -90 - y);
-  vertex(0, -80 - y);
-  vertex(20, -80 - y);
-  endShape(CLOSE);
-  pop();
+      if (x1rans[i-1] > 0){
+        x2ran = random(-20, 0);
+      } else {
+        x2ran = random(20);
+      }
+      if (y1rans[i-1] > 0){
+        y2ran = random(-20, 0);
+      } else {
+        y2ran = random(20);
+      }
+      
+      line(x1, y1, x2, y2);
+      x1s.push(x2);
+      y1s.push(y2);
+      x1rans.push(x2ran);
+      y1rans.push(y2ran);
+      
+      x1 = x1s[i];
+      y1 = y1s[i];
+      
+      x2 = pts[i+1].x+x2ran;
+      y2 = pts[i+1].y+y2ran;
+      
+    } 
+    pop(); 
+    }
+
+  } 
+ 
 }
